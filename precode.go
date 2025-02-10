8b1a65da5b2fb6a2ee5e51b7a6f91d6a6a5365f9
SomeBody <rickandmortyqqq@yandex.ru>
2025-02-10 09:50:16 +0300

    last_last

--- precode.go
+++ precode.go
@@ -151,7 +151,7 @@ func main() {
 	r.Post("/tasks", addTask)
 	r.Get("/tasks/{id}", getTask)
 	r.Delete("/tasks/{id}", deleteTask)
-
+	//
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
 		return
